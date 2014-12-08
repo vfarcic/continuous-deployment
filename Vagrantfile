d1669fe069ea90ccd9e3450a54f12bb4079f1846
Viktor Farcic <viktor@farcic.com>
2014-12-08 17:57:47 +0100

    Added article reference

--- Vagrantfile
+++ Vagrantfile
@@ -5,8 +5,8 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "yungsang/coreos"
-  # config.vm.provision "shell", inline: "nohup etcd & >/dev/null 2>&1", run: "always"
-  # config.vm.provision "shell", path: "bootstrap.sh"
+  config.vm.provision "shell", inline: "nohup etcd & >/dev/null 2>&1", run: "always"
+  config.vm.provision "shell", path: "bootstrap.sh"
   config.vm.network :forwarded_port, host: 8000, guest: 80
   config.vm.network :forwarded_port, host: 9000, guest: 9000
   config.vm.synced_folder ".", "/vagrant"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
+echo "Starting bootstrap.sh..."
+
 # confd
 
+echo "Starting confd..."
 wget -O confd https://github.com/kelseyhightower/confd/releases/download/v0.6.3/confd-0.6.3-linux-amd64
 cp confd /opt/bin/.
 chmod +x /opt/bin/confd
 mkdir -p /etc/confd/{conf.d,templates}
 cp /vagrant/bdd_assistant.toml /etc/confd/conf.d/.
 cp /vagrant/bdd_assistant.conf.tmpl /etc/confd/templates/.
+echo "Finished confd..."
 
 # etcd
 
 systemctl start etcd.service
 
 # nginx
+echo "Starting nginx..."
 mkdir -p /etc/nginx/{sites-enabled,certs-enabled}
 mkdir -p /var/log/nginx
 cp /vagrant/nginx.service /etc/systemd/system/nginx.service
 systemctl enable /etc/systemd/system/nginx.service
 systemctl start nginx.service
+echo "Finished nginx..."
 
 # BDD Assistant
+echo "Starting BDD Assistant..."
 cp /vagrant/bdd_assistant.service /etc/systemd/system/bdd_assistant_blue@9001.service
 cp /vagrant/bdd_assistant.service /etc/systemd/system/bdd_assistant_green@9002.service
 systemctl enable /etc/systemd/system/bdd_assistant_blue@9001.service
@@ -30,4 +37,6 @@ etcdctl set /bdd-assistant/instance none
 chmod 744 /vagrant/deploy_bdd_assistant.sh
 cp /vagrant/deploy_bdd_assistant.sh /opt/bin/.
 deploy_bdd_assistant.sh
+echo "Finished BDD Assistant..."
 
+echo "Finished bootstrap.sh..."
